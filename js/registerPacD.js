57d5ceb7001d894e29255a9320764af46101ba14
Diego Saleh <diego.alesaleh1@gmail.com>
2023-11-28 01:27:39 -0300

    Registro y login de paciente completo

--- js/registerPacD.js
+++ js/registerPacD.js
@@ -3,7 +3,7 @@ const btnSignUp = document.getElementById("sign-up");
 const formRegister = document.querySelector(".registro");
 const formRlogin = document.querySelector(".login");
 
-btnSignUp.addEventListener("click", e => {
+btnSignUp.addEventListener("click", (e) => {
     formRegister.classList.add("hide");
     formRlogin.classList.remove("hide")
 });
@@ -15,47 +15,28 @@ btnSignIn.addEventListener("click", e => {
 
 
 const form = document.querySelector(".form");
-
-alertify.set('notifier','position', 'top-center')
+alertify.set("notifier", "position", "top-center")
 
 let arrayLS = [];
 
+let nombre, dni, correo, contrasenia, controlContrasenia;
 
-function registrarse(event){
-    event.preventDefault()
-    const nombre = document.getElementById('nombre').value;
-    const dni = document.getElementById('dni').value
-    const correo = document.getElementById('email').value
-    const contrasenia = document.getElementById('password').value
-    const controlContrasenia = document.getElementById('controlPassword').value
-    if (!nombre.match(/^[A-ZÑa-zñáéíóúÁÉÍÓÚ'° ]{8,18}$/)){
-        alertify.error("El nombre ingresado no es válido, debe contener entre 8 y 18 caracteres alfanuméricos");
-        return;
-    };
-    if (!dni.match(/^\d{7,8}$/)) {
-        alertify.error("El DNI ingresado no es válido, debe contener entre 7 y 8 dígitos numéricos");
+let pacientes = JSON.parse(localStorage.getItem("pacientePendiente")) || [];
+let dnisRegistro = new Set(pacientes.map(p => p.dni));
+let correoRegistro = new Set(pacientes.map(p => p.email));
+let contraseniaRegistro = new Set(pacientes.map(p => p.password))
+
+function registrarse(event) {
+    event.preventDefault();
+    if (!recibirInputs()) {
+        // Las validaciones fallaron, no continuar con el registro
         return;
     }
-    if (!correo.match(/([a-z]\w+@[a-z]+\.[a-z]{2,5})/)) {
-        alertify.error("El mail ingresado no es valido, asegurese de que su email contenga letras en minúsculas, un @ y un .")
-        return
-    };
-    if (!contrasenia.match(/^[A-Z](?=\w*\d)(?=\w*[a-z])\S{8,16}/)){
-        alertify.error("La contraseña ingresada no es valida, asegúrese de que la primer letra sea mayúscula y algun número. La contraseña puede contener entre 8-16 carácteres alfanuméricos")
-        return
-    };
-    if (contrasenia===controlContrasenia){
-    }else{
-        alertify.error("Las contraseñas no coinciden")
-        return
-    };
-    const valorLS = localStorage.getItem("paciente");
+    const valorLS = localStorage.getItem("pacientPendiente");
     arrayLS = [];
-
     if (valorLS) {
         arrayLS = JSON.parse(valorLS);
     }
-
     const nuevoPaciente = {
         nombre: nombre,
         dni: dni,
@@ -68,22 +49,80 @@ function registrarse(event){
     arrayLS.push(nuevoPaciente);
 
     // Guardar el array actualizado en el localStorage
-    localStorage.setItem("paciente", JSON.stringify(arrayLS));
+    localStorage.setItem("pacientePendiente", JSON.stringify(arrayLS));
 
     console.log(arrayLS);
+    alertify.warning(`Su registro se está procesando. La aprobación será enviada a ${correo}`)
+    form.reset();
+
+    // dnisRegistro.add(dni);
+    // localStorage.setItem("dnisRegistro", JSON.stringify(Array.from(dnisRegistro)));
+    // localStorage.setItem("correoRegistro", JSON.stringify(Array.from(correoRegistro)));
 }
-
-// const resetearValores = (() => {
-//     nombre.
-// }) 
-
 
 function mostrarContraseña(passwordId, eyeId) {
     const passwordInput = document.getElementById(passwordId);
-    const eyeIcon = document.getElementById(eyeId).querySelector('i');
-    passwordInput.type = (passwordInput.type === 'password') ? 'text' : 'password';
-    const iconClass = (passwordInput.type === 'password') ? 'fa-eye-slash' : 'fa-eye';
-    eyeIcon.className = `fas ${iconClass}`;
+    const eyeIcon = document.getElementById(eyeId).querySelector("i");
+    passwordInput.type = (passwordInput.type === "password") ? "text" : "password";
+    const iconClass = (passwordInput.type === "password") ? "fa-eye-slash" : "fa-eye";
+    eyeIcon.className = `fa-solid ${iconClass}`;
 }
 
+const recibirInputs = () => {
+    nombre = document.getElementById("nombre").value;
+    dni = document.getElementById("dni").value;
+    correo = document.getElementById("email").value;
+    contrasenia = document.getElementById("password").value;
+    controlContrasenia = document.getElementById("controlPassword").value;
+    if (dnisRegistro.has(dni)) {
+        alertify.error("Este DNI ya está registrado. Por favor, ingrese otro DNI.");
+        return false;
+    }
+    if (correoRegistro.has(correo)) {
+        alertify.error("Este correo electrónico ya está registrado. Por favor, ingrese otro correo electrónico.");
+        return false;
+    }
+    if (!nombre.match(/^[A-ZÑa-zñáéíóúÁÉÍÓÚ"° ]{8,18}$/)) {
+        alertify.error("El nombre ingresado no es válido, debe contener entre 8 y 18 caracteres alfanuméricos");
+        return false;
+    };
+    if (!dni.match(/^\d{7,8}$/)) {
+        alertify.error("El DNI ingresado no es válido, debe contener entre 7 y 8 dígitos numéricos");
+        return false;
+    }
+    if (!correo.match(/([a-z]\w+@[a-z]+\.[a-z]{2,5})/)) {
+        alertify.error("El correo electrónico ingresado no es valido, asegurese de que su correo electrónico contenga letras en minúsculas, un @ y un .")
+        return false;
+    };
+    if (!contrasenia.match(/^[A-Z](?=\w*\d)(?=\w*[a-z])\S{8,16}/)) {
+        alertify.error("La contraseña ingresada no es valida, asegúrese de que la primer letra sea mayúscula y algun número. La contraseña puede contener entre 8-16 carácteres alfanuméricos")
+        return false;
+    };
+    if (contrasenia === controlContrasenia) {
+    } else {
+        alertify.error("Las contraseñas no coinciden")
+        return false;
+    };
+    dnisRegistro.add(dni);
+    correoRegistro.add(correo);
+    return true;
+}
+
+const iniciarSesion = (event) => {
+    event.preventDefault();
+    const emailLog = document.getElementById("email2").value;
+    const passwordLog = document.getElementById("password2").value;
+    const user = pacientes.find((item) => item.email === emailLog && item.password === passwordLog);
+    if (user) {
+        // Usuario encontrado, redirigir según el estado del usuario
+        if (user.isAdmin) {
+            window.location.href = "registerPacD.html";
+        } else {
+            window.location.href = "registerPacD.html";
+        }
+    } else {
+        // Usuario no encontrado, mostrar mensaje de error
+        alertify.error("El usuario o la contraseña ingresada no son correctos");
+    }
+};
 
