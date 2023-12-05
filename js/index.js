e36b2080fa67f0b4fae7c7cfd5793478a5f5b335
MariaBelenCastro <mariabelencastro158@gmail.com>
2023-12-04 22:28:07 -0300

    Cambio de rutas en index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 const user = JSON.parse(localStorage.getItem("userLogged")) || false; //en el caso de que haya algo dentro del local storage se guarda en login sino es false
 
 if(!user){
-    window.location.href = "inicioSesion.html";
+    window.location.href = "registerPacD.html";
 }
 const logOut = document.getElementById("logOut");
 
 logOut.addEventListener("click", ()=>{   //esto agregamos para cerrar sesion
     alert("Hasta pronto!");
     localStorage.removeItem("userLogged"); //me borra la cuenta iniciada del localStorage cuando cierre sesion
-    window.location.href = "inicioSesion.html";
+    window.location.href = "registerPacD.html";
 })
