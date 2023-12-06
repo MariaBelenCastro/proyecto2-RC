5269ee68ab596aaea00e562dcbff87c7a17d8e68
Diego Saleh <diego.alesaleh1@gmail.com>
2023-12-06 18:08:20 -0300

    Agrego pacientes, medicos y turnos a app.js

--- app.js
+++ app.js
@@ -63,11 +63,98 @@ medico = [
         especialidad:'Oncologo',
         horario:['09:00am', '13:00pm', '16:00pm', '19:00pm'],
         dia:['Lunes','Jueves','Miercoles']
+    },
+    {
+        nombre: 'Ana',
+        apellido: 'Fernandez',
+        dni: '78901234',
+        email: 'anafernandez@gmail.com',
+        password: 'Ana1234',
+        matricula: '56789',
+        especialidad: 'Cardiologo',
+        horario: ['08:00am', '12:00pm', '15:00pm', '18:00pm'],
+        dia: ['Martes', 'Jueves', 'Sabado']
+    },
+    {
+        nombre: 'Luis',
+        apellido: 'Martinez',
+        dni: '56789012',
+        email: 'luismartinez@gmail.com',
+        password: 'Luis5678',
+        matricula: '98765',
+        especialidad: 'Dermatologo',
+        horario: ['10:00am', '14:00pm', '17:00pm', '20:00pm'],
+        dia: ['Lunes', 'Miercoles', 'Viernes']
+    },
+    {
+        nombre: 'Maria',
+        apellido: 'Rodriguez',
+        dni: '34567890',
+        email: 'mariarodriguez@gmail.com',
+        password: 'Maria7890',
+        matricula: '12345',
+        especialidad: 'Gastroenterologo',
+        horario: ['11:00am', '15:00pm', '18:00pm', '21:00pm'],
+        dia: ['Martes', 'Jueves', 'Sabado']
+    },
+    {
+        nombre: 'Carlos',
+        apellido: 'Gomez',
+        dni: '12345678',
+        email: 'carlosgomez@gmail.com',
+        password: 'Carlos5678',
+        matricula: '54321',
+        especialidad: 'Neurologo',
+        horario: ['08:00am', '12:00pm', '15:00pm', '18:00pm'],
+        dia: ['Lunes', 'Miercoles', 'Viernes']
+    },
+    {
+        nombre: 'Laura',
+        apellido: 'Hernandez',
+        dni: '87654321',
+        email: 'laurahernandez@gmail.com',
+        password: 'Laura1234',
+        matricula: '98765',
+        especialidad: 'Pediatra',
+        horario: ['10:00am', '14:00pm', '17:00pm', '20:00pm'],
+        dia: ['Martes', 'Jueves', 'Sabado']
+    },
+    {
+        nombre: 'Javier',
+        apellido: 'Diaz',
+        dni: '56781234',
+        email: 'javierdiaz@gmail.com',
+        password: 'Javier5678',
+        matricula: '43210',
+        especialidad: 'Oncologo',
+        horario: ['11:00am', '15:00pm', '18:00pm', '21:00pm'],
+        dia: ['Lunes', 'Miercoles', 'Viernes']
+    },
+    {
+        nombre: 'Silvia',
+        apellido: 'Torres',
+        dni: '87654312',
+        email: 'silviatorres@gmail.com',
+        password: 'Silvia1234',
+        matricula: '54321',
+        especialidad: 'Cardiologo',
+        horario: ['08:00am', '12:00pm', '15:00pm', '18:00pm'],
+        dia: ['Martes', 'Jueves', 'Sabado']
+    },
+    {
+        nombre: 'Ricardo',
+        apellido: 'Ramirez',
+        dni: '12348765',
+        email: 'ricardoramirez@gmail.com',
+        password: 'Ricardo5678',
+        matricula: '87654',
+        especialidad: 'Dermatologo',
+        horario: ['10:00am', '14:00pm', '17:00pm', '20:00pm'],
+        dia: ['Lunes', 'Miercoles', 'Viernes']
     }
-    
-  ];
+];
 
-  pacientePendiente =[
+pacientePendiente =[
     {
         nombre:'Ramiro',
         apellido:'Jodar',
@@ -102,6 +189,61 @@ medico = [
         email:'Augusto1@hotmail.com',
         password:'Augusto1234',
         dni:'27895645'
+    },
+    {
+        nombre: 'Carlos',
+        apellido: 'Gomez',
+        dni: '12345678',
+        email: 'carlosgomez@gmail.com',
+        password: 'Carlos5678',
+        matricula: '54321',
+        especialidad: 'Neurologo',
+        horario: ['08:00am', '12:00pm', '15:00pm', '18:00pm'],
+        dia: ['Lunes', 'Miercoles', 'Viernes']
+    },
+    {
+        nombre: 'Laura',
+        apellido: 'Hernandez',
+        dni: '87654321',
+        email: 'laurahernandez@gmail.com',
+        password: 'Laura1234',
+        matricula: '98765',
+        especialidad: 'Pediatra',
+        horario: ['10:00am', '14:00pm', '17:00pm', '20:00pm'],
+        dia: ['Martes', 'Jueves', 'Sabado']
+    },
+    {
+        nombre: 'Javier',
+        apellido: 'Diaz',
+        dni: '56781234',
+        email: 'javierdiaz@gmail.com',
+        password: 'Javier5678',
+        matricula: '43210',
+        especialidad: 'Oncologo',
+        horario: ['11:00am', '15:00pm', '18:00pm', '21:00pm'],
+        dia: ['Lunes', 'Miercoles', 'Viernes']
+    },
+    {
+        nombre: 'Silvia',
+        apellido: 'Torres',
+        dni: '87654312',
+        email: 'silviatorres@gmail.com',
+        password: 'Silvia1234',
+        matricula: '54321',
+        especialidad: 'Cardiologo',
+        horario: ['08:00am', '12:00pm', '15:00pm', '18:00pm'],
+        dia: ['Martes', 'Jueves', 'Sabado']
+    },
+    {
+        nombre: 'Ricardo',
+        apellido: 'Ramirez',
+        dni: '12348765',
+        email: 'ricardoramirez@gmail.com',
+        password: 'Ricardo5678',
+        matricula: '87654',
+        especialidad: 'Dermatologo',
+        horario: ['10:00am', '14:00pm', '17:00pm', '20:00pm'],
+        dia: ['Lunes', 'Miercoles', 'Viernes']
     }
   ] 
 
